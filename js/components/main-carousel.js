f40220e4d927db61e4367f2ed98327c5580adf39
Thiago <thiagodavidpd@gmail.com>
2021-05-16 19:24:54 -0300

    Added background-image on Sobre section

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -5,14 +5,17 @@ const elements = document.querySelector('.elements');
 var pixels = 0;
 
 const buttonLeft = document.querySelector('.button-arrow.-left');
-
+    
 buttonRight.addEventListener('click', function() {
     pixels = pixels + 170;
     elements.style = `transform: translateX(${pixels}px)`;
-
+    console.log(pixels)
 })
+
 
 buttonLeft.addEventListener('click', function(){
     pixels = pixels - 170;
     elements.style= `transform: translateX(${pixels}px)`
+    console.log(pixels)
 })
+
