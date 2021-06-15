151cc67d4698cfbaec6801d18678839e3890edb7
Thiago <thiago_galaxy@hotmail.com>
2021-06-15 15:20:49 -0300

    Reverse carousel direction

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -6,14 +6,14 @@ var pixels = 0;
 
 const buttonLeft = document.querySelector('.button-arrow.-left');
     
-buttonRight.addEventListener('click', function() {
+buttonLeft.addEventListener('click', function() {
     pixels = pixels + 170;
     elements.style = `transform: translateX(${pixels}px)`;
     console.log(pixels)
 })
 
 
-buttonLeft.addEventListener('click', function(){
+buttonRight.addEventListener('click', function(){
     pixels = pixels - 170;
     elements.style= `transform: translateX(${pixels}px)`
     console.log(pixels)
